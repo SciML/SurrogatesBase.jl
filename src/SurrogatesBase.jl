7902ef7ce1dcd48d3421949fccefcc6a61beaf39
Samuel Belko <samuelbelko@protonmail.com>
2023-09-15 13:45:45 +0200

    refine

--- src/SurrogatesBase.jl
+++ src/SurrogatesBase.jl
@@ -3,54 +3,53 @@ module SurrogatesBase
 import Statistics: mean, var
 import Base: rand
 
-# AbstractSurrogate interface
 export AbstractSurrogate,
     add_point!,
     update_hyperparameters!, hyperparameters,
     posterior, mean, var, rand, logpdf
 
 """
-    abstract type AbstractSurrogate end
+    abstract type AbstractSurrogate{D, R} end
 
-An abstract type for defining surrogate interface.
+An abstract type for surrogate interface, parametrized by domain type `D` and
+range type `R` of the underlying function that is being approximated.
 
-    (s::AbstractSurrogate)(x::AbstractVector)
+    (s::AbstractSurrogate{D, R})(x::D) where {D, R}
 
-Subtypes of `AbstractSurrogate` need to be callable with input points `x` that returns
-an approximation at `x`, i.e., evaluates the surrogate at `x`.
+Subtypes of `AbstractSurrogate` need to be callable with input points `x` that
+evaluates the surrogate at `x`.
 
  # Examples
  ```jldoctest
- julia> struct ZeroSurrogate <: AbstractSurrogate end
+ julia> struct ZeroSurrogate{D, R} <: AbstractSurrogate{D, R} end
 
- julia> (::ZeroSurrogate)(x::AbstractVector) = 0
+ julia> (::ZeroSurrogate{D,R})(x::D) where {D,R} = 0
 
- julia> s = ZeroSurrogate()
- ZeroSurrogate()
+ julia> s = ZeroSurrogate{Int, Int}()
+ ZeroSurrogate{Int64, Int64}()
 
- julia> s(rand(5)) == 0
+ julia> s(4) == 0
  true
  ```
  """
-abstract type AbstractSurrogate <: Function end
+abstract type AbstractSurrogate{D, R} <: Function end
 
 """
-    add_point!(s::AbstractSurrogate, new_x::AbstractVector, new_y::Number)
-    add_point!(s::AbstractSurrogate, new_x::Union{AbstractVector, Number}, new_y::Number)
+    add_point!(s::AbstractSurrogate{D, R}, new_x::D, new_y::R) where {D, R}
 
 Add an evaluation `new_y` at point `new_x` to the surrogate.
 """
 function add_point! end
 """
-    add_point!(s::AbstractSurrogate, new_xs::AbstractVector{<:AbstractVector}, new_ys::AbstractVector)
+    add_point!(s::AbstractSurrogate{D, R}, new_xs::AbstractVector{D}, new_ys::AbstractVector{R}) where {D, R}
 
 Add evaluations `new_ys` at points `new_xs` to the surrogate.
 
 Use `add_point!(s, eachslice(X, dims = 2), new_ys)` if `X` is a matrix.
 """
-function add_point!(s::AbstractSurrogate,
-    new_xs::AbstractVector{<:AbstractVector},
-    new_ys::AbstractVector)
+function add_point!(s::AbstractSurrogate{D, R},
+    new_xs::AbstractVector{D},
+    new_ys::AbstractVector{R}) where {D, R}
     add_point!.(Ref(s), new_xs, new_ys)
 end
 
@@ -74,7 +73,7 @@ See also [`update_hyperparameters!`](@ref).
 function hyperparameters end
 
 """
-    posterior(s::AbstractSurrogate, xs::AbstractVector{<:AbstractVector})
+    posterior(s::AbstractSurrogate{D, R}, xs::AbstractVector{D}) where {D, R}
 
 Return a joint posterior at points `xs`.
 
@@ -82,58 +81,58 @@ Use `posterior(s, eachslice(X, dims = 2))` if `X` is a matrix.
 """
 function posterior end
 """
-    posterior(s::AbstractSurrogate, x::AbstractVector)
+    posterior(s::AbstractSurrogate{D, R}, x::D) where {D, R}
 
 Return posterior at point `x`.
 """
-posterior(s::AbstractSurrogate, x::AbstractVector) = posterior(s, [x])
+posterior(s::AbstractSurrogate{D, R}, x::D) where {D, R} = posterior(s, [x])
 
 """
-    mean(s::AbstractSurrogate, x::AbstractVector)
+    mean(s::AbstractSurrogate{D, R}, x::D) where {D, R}
 
 Return mean at point `x`.
 """
 function mean end
 """
-    mean(s::AbstractSurrogate, xs::AbstractVector{<:AbstractVector})
+    mean(s::AbstractSurrogate{D, R}, xs::AbstractVector{D}) where {D, R}
 
 Return a vector of means at points `xs`.
 
 Use `mean(s, eachslice(X, dims = 2))` if `X` is a matrix.
 """
-function mean(s::AbstractSurrogate, xs::AbstractVector{<:AbstractVector})
+function mean(s::AbstractSurrogate{D, R}, xs::AbstractVector{D}) where {D, R}
     mean.(Ref(s), xs)
 end
 
 """
-    var(s::AbstractSurrogate, x::AbstractVector)
+    var(s::AbstractSurrogate{D, R}, x::D) where {D, R}
 
 Return variance at point `x`.
 """
 function var end
 
 """
-    var(s::AbstractSurrogate, xs::AbstractVector{<:AbstractVector})
+    var(s::AbstractSurrogate{D, R}, xs::AbstractVector{D}) where {D, R}
 
 Return a vector of variances at points `xs`.
 """
-function var(s::AbstractSurrogate, xs::AbstractVector{<:AbstractVector})
+function var(s::AbstractSurrogate{D, R}, xs::AbstractVector{D}) where {D, R}
     var.(Ref(s), xs)
 end
 
 """
-    rand(s::AbstractSurrogate, xs::AbstractVector{<:AbstractVector})
+    rand(s::AbstractSurrogate{D, R}, xs::AbstractVector{D}) where {D, R}
 
 Return a sample from the joint posterior at points `xs`.
 """
 function rand end
 
 """
-    rand(s::AbstractSurrogate, x::AbstractVector)
+    rand(s::AbstractSurrogate{D,R}, x::D)
 
 Return a sample from the posterior distribution at a point `x`.
 """
-rand(s::AbstractSurrogate, x::AbstractVector) = only(rand(s::AbstractSurrogate, [x]))
+rand(s::AbstractSurrogate{D, R}, x::D) where {D, R}= only(rand(s::AbstractSurrogate, [x]))
 
 """
     logpdf(s::AbstractSurrogate)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,48 +1,34 @@
 using SurrogatesBase
-import SurrogatesBase: add_point!,
-    update_hyperparameters!, hyperparameters, mean, var
+import SurrogatesBase:
+    add_point!,
+    update_hyperparameters!, hyperparameters,
+    posterior, mean, var, rand, logpdf
+
 using Test
 using LinearAlgebra
 
-mutable struct DummySurrogate{X, Y} <: AbstractSurrogate
-    xs::Vector{X}
-    ys::Vector{Y}
+mutable struct DummySurrogate{D, R} <: AbstractSurrogate{D, R}
+    xs::Vector{D}
+    ys::Vector{R}
 end
 
 # return y value of the closest ξ in xs to x
-(s::DummySurrogate)(x::AbstractVector) = s.ys[argmin(norm(x - ξ) for ξ in s.xs)]
-function add_point!(s::DummySurrogate, new_x::AbstractVector, new_y::Number)
+(s::DummySurrogate{D, R})(x::D) where {D, R} = s.ys[argmin(norm(x - ξ) for ξ in s.xs)]
+function add_point!(s::DummySurrogate{D, R}, new_x::D, new_y::R) where {D, R}
     push!(s.xs, new_x)
     push!(s.ys, new_y)
 end
 
 # dummy mean at point
-mean(s::DummySurrogate, x::AbstractVector{<:Number}) = x
+mean(s::DummySurrogate{D, R}, x::D) where {D, R} = norm(x)
+# dummy variance at point
+var(s::DummySurrogate{D,R}, x::D) where {D, R} = 5
 # dummy variance at more points
-var(s::DummySurrogate, xs::AbstractVector{AbstractVector{<:Number}}) = (x -> x^2).(xs)
-
-mutable struct HyperparameterDummySurrogate{X, Y} <: AbstractSurrogate
-    xs::Vector{X}
-    ys::Vector{Y}
-    θ::NamedTuple
-end
-
-# return y value of the closest ξ in xs to x, in p-norm where p is a hyperparameter
-(s::HyperparameterDummySurrogate)(x) = s.ys[argmin(norm(x - ξ, s.θ.p) for ξ in s.xs)]
-function add_point!(s::HyperparameterDummySurrogate, new_x::AbstractVector, new_y::Number)
-    push!(s.xs, new_x)
-    push!(s.ys, new_y)
-end
-supports_hyperparameters(s::HyperparameterDummySurrogate) = true
-hyperparameters(s::HyperparameterDummySurrogate) = s.θ
-function update_hyperparameters!(s::HyperparameterDummySurrogate, prior)
-    # "hyperparmeter optimization"
-    s.θ = (; p = (s.θ.p + prior.p) / 2)
-end
-
+var(s::DummySurrogate{D, R}, xs::Vector{D}) where {D, R} = ones(Base.length(xs))
+# dummy logpdf
+logpdf(s::DummySurrogate, x) = 0.5
 
 @testset "add_point! with broadcasting implementation" begin
-    # use DummySurrogate
     d = DummySurrogate(Vector{Vector{Float64}}(), Vector{Int}())
     add_point!(d, [1.9, 2.1], 5)
     add_point!(d, [10.3, 0.1], 9)
@@ -52,29 +38,65 @@ end
     @test d([0.0, -9.9]) == 3
 end
 
-@testset "mean with broadcasting" begin
-      # use DummySurrogate
+@testset "var" begin
+    # use DummySurrogate
     d = DummySurrogate(Vector{Vector{Float64}}(), Vector{Int}())
     add_point!(d, [1.9, 2.1], 5)
-    @test mean(d, [[1., 4. ], [5.,6.]]) == [[1., 4. ], [5.,6.]]
+    @test var(d, [1.0, 4.0]) == 5
+    @test var(d, [[1.0, 4.0], [5.0, 6.0]]) == ones(2)
 end
 
-@testset "default implementations" begin
+@testset "mean" begin
     # use DummySurrogate
-    d = DummySurrogate(Vector{Vector{Float64}}(), Vector{Float64}())
-    add_point!(d, [1.9, 2.1], 5.0)
-    add_point!(d, [10.3, 0.1], 9.0)
+    d = DummySurrogate(Vector{Vector{Float64}}(), Vector{Int}())
+    add_point!(d, [1.9, 2.1], 5)
+    @test isapprox( mean(d, [1.0, 4.0]),  norm([1.0, 4.0]))
+    @test isapprox( mean(d, [[1.0, 4.0], [5.0, 6.0]]) , [norm([1.0, 4.0]), norm([5.0, 6.0])])
+end
 
-    @test d([2.0, 2.0]) == 5.0
+@testset "logpdf" begin
+    d = DummySurrogate(Vector{Vector{Float64}}(), Vector{Float64}())
+    add_point!(d, [1.9, 2.1], 5.9)
+    @test logpdf(d) == 0.5
+end
+
+@testset "not implemented methods throw an error" begin
+    d = DummySurrogate(Vector{Vector{Float64}}(), Vector{Float64}())
+    add_point!(d, [1.9, 2.1], 5.9)
     @test_throws MethodError hyperparameters(d)
     @test_throws MethodError update_hyperparameters!(d, 5)
     @test_throws MethodError posterior(d, 5)
+    @test_throws MethodError rand(d, 5)
+end
+
+@testset "add_point! in 1D surrogate" begin
+    d = DummySurrogate(Vector{Float64}(), Vector{Float64}())
+    add_point!(d, 1, 3)
+    add_point!(d, [2,3,4], [4,5,6])
+    @test length(d.x) == 5
+end
+
+
+mutable struct HyperparameterDummySurrogate{D, R} <: AbstractSurrogate{D, R}
+    xs::Vector{D}
+    ys::Vector{R}
+    θ::NamedTuple
+end
+# return y value of the closest ξ in xs to x, in p-norm where p is a hyperparameter
+(s::HyperparameterDummySurrogate)(x) = s.ys[argmin(norm(x - ξ, s.θ.p) for ξ in s.xs)]
+function add_point!(s::HyperparameterDummySurrogate{D, R}, new_x::D, new_y::R) where {D, R}
+    push!(s.xs, new_x)
+    push!(s.ys, new_y)
+end
+hyperparameters(s::HyperparameterDummySurrogate) = s.θ
+function update_hyperparameters!(s::HyperparameterDummySurrogate, prior)
+    # "hyperparmeter optimization"
+    s.θ = (; p = (s.θ.p + prior.p) / 2)
 end
 
 @testset "hyperparameter interface" begin
-    # use HyperparameterDummySurrogate
     hd = HyperparameterDummySurrogate(Vector{Vector{Float64}}(),
-        Vector{Float64}(),
+         Vector{Float64}(),
         (; p = 2))
     add_point!(hd, [1.9, 2.1], 5.0)
     add_point!(hd, [10.3, 0.1], 9.0)
