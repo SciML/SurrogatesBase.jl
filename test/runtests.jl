b5083454e1a17e29676f1010ad7a21da07bea820
Samuel Belko <samuelbelko@protonmail.com>
2023-12-01 12:15:10 +0100

    format

--- test/runtests.jl
+++ test/runtests.jl
@@ -37,7 +37,7 @@ end
 @testset "add_points!" begin
     # use DummySurrogate
     d = DummySurrogate(Vector{Vector{Float64}}(), Vector{Int}())
-    add_points!(d, [[10.3, 0.1],[1.9, 2.1]], [5,6])
+    add_points!(d, [[10.3, 0.1], [1.9, 2.1]], [5, 6])
     add_points!(d, [[-0.3, 9.9], [-0.1, -10.0]], [1, 3])
     @test length(d.xs) == 4
     @test d([0.0, -9.9]) == 3
@@ -59,7 +59,7 @@ end
     hd = HyperparameterDummySurrogate(Vector{Vector{Float64}}(),
         Vector{Float64}(),
         (; p = 2))
-    add_points!(hd, [[1.9, 2.1],[10.3, 0.1]], [5.0, 9.])
+    add_points!(hd, [[1.9, 2.1], [10.3, 0.1]], [5.0, 9.0])
 
     @test hyperparameters(hd).p == 2
     update_hyperparameters!(hd, (; p = 4))
@@ -84,15 +84,17 @@ function FiniteDummyStochasticSurrogate(s, xs)
     return FiniteDummyStochasticSurrogate(zeros(length(xs)) .+ sum(s.ys))
 end
 
-SurrogatesBase.finite_posterior(s::DummyStochasticSurrogate, xs) = FiniteDummyStochasticSurrogate(s, xs)
+function SurrogatesBase.finite_posterior(s::DummyStochasticSurrogate, xs)
+    FiniteDummyStochasticSurrogate(s, xs)
+end
 
 @testset "finite_posterior" begin
     # use HyperparameterDummySurrogate
     ss = DummyStochasticSurrogate(Vector{Vector{Float64}}(),
         Vector{Float64}())
 
-    add_points!(ss, [[1.9, 2.1],[10.3, 0.1]], [5.0, 9.])
-    m = Statistics.mean(finite_posterior(ss, [[3.5,2.], [4.,5.],[1.,67.]]))
+    add_points!(ss, [[1.9, 2.1], [10.3, 0.1]], [5.0, 9.0])
+    m = Statistics.mean(finite_posterior(ss, [[3.5, 2.0], [4.0, 5.0], [1.0, 67.0]]))
     @test length(m) == 3
-    @test m[1] ≈ 14.
+    @test m[1] ≈ 14.0
 end
