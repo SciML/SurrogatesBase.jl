3641d89f36f34f67bab444768b14b5779c865b29
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-02-24 04:16:21 -0500

    Build a proper documentation

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using Documenter, SurrogatesBase
+
+cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
+cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
+
+pages = [
+    "Home" => "index.md",
+    "interface.md",
+    "api.md"
+]
+
+ENV["GKSwstype"] = "100"
+
+makedocs(modules = [SurrogatesBase],
+    sitename = "SurrogatesBase.jl",
+    clean = true,
+    doctest = false,
+    linkcheck = true,
+    format = Documenter.HTML(assets = ["assets/favicon.ico"],
+        canonical = "https://docs.sciml.ai/SurrogatesBase/stable/"),
+    pages = pages)
+
+deploydocs(repo = "github.com/SciML/SurrogatesBase.jl"; push_preview = true)
